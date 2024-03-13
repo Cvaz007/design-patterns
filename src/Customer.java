cb2eeaf21200e389953b32feb263b10c5dc6ba3a
David <David@DESKTOP-TTVGEKV>
2024-03-13 15:28:26 -0500

    builder

---
+++ src/Customer.java
@@ -0,0 +1,37 @@
+public class Customer {
+
+    private String name;
+    private String phoneNunmber;
+    private String address;
+    private String email;
+    private PaymentMethod paymentMethod;
+
+
+    public Customer(String name, String phoneNunmber, String address, String email, PaymentMethod paymentMethod) {
+        this.name = name;
+        this.phoneNunmber = phoneNunmber;
+        this.address = address;
+        this.email = email;
+        this.paymentMethod = paymentMethod;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPhoneNunmber() {
+        return phoneNunmber;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public PaymentMethod getPaymentMethod() {
+        return paymentMethod;
+    }
+}
