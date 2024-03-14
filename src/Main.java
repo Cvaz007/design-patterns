813f94da2fc93d66c18a905624445dbee56ad02a
David <David@DESKTOP-TTVGEKV>
2024-03-14 16:08:49 -0500

    singleton

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+import java.util.Date;
+
+public class Main {
+    public static void main(String[] args) {
+        Employee employee1 = new Employee("jairo", 1);
+        Employee employee2 = new Employee("Maria", 2);
+
+
+        AccessControlSystem system = AccessControlSystem.getInstance();
+
+
+        system.registerEntry(employee1, new Date());
+        system.registerExit(employee1, new Date());
+        system.registerEntry(employee2, new Date());
+        system.registerExit(employee2, new Date());
+
+        system.generateReport();
+    }
+}
---
+++ src/Record.java
@@ -0,0 +1,28 @@
+import java.util.Date;
+
+class Record {
+    private Employee employee;
+    private Date entry;
+    private Date exit;
+
+    public Record(Employee employee, Date entry) {
+        this.employee = employee;
+        this.entry = entry;
+    }
+
+    public Employee getEmployee() {
+        return employee;
+    }
+
+    public Date getEntry() {
+        return entry;
+    }
+
+    public Date getExit() {
+        return exit;
+    }
+
+    public void setExit(Date exit) {
+        this.exit = exit;
+    }
+}
